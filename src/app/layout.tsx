fc3fe4775846633fbf9c57f448a910197e153850
yellow <gphotosbot@gmail.com>
2025-10-02 01:59:41 +0530

    update name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Orange Plum",
-  description: "Sigma Plum",
+  title: "Candle",
+  description: "Blow your candle",
 };
 
 export default function RootLayout({
